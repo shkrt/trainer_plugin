07653a65477143aaa424d840a7469522f0f0f547
Shkrt <zxcgpppmn@gmail.com>
2016-02-08 18:48:29 +0300

    move user class to settings

--- app/controllers/trainer_plugin/reports_controller.rb
+++ app/controllers/trainer_plugin/reports_controller.rb
@@ -1,7 +1,9 @@
 module TrainerPlugin
   class ReportsController < ApplicationController
     def main
-      @users = User.all
+      usr_class = TrainerPlugin.config.user_class.constantize
+      @users = usr_class.all
+      @user_key = "#{usr_class.downcase}_id".to_sym
     end
   end
 end
--- lib/trainer_plugin.rb
+++ lib/trainer_plugin.rb
@@ -1,6 +1,19 @@
 require "trainer_plugin/engine"
 
 module TrainerPlugin
+  class Configuration
+    attr_accessor :user_class
+  end
+
+  class << self
+    attr_reader :config
+
+    def configure
+      @config = Configuration.new
+      yield @config
+    end
+  end
+
   def track(name, id)
     AhoyTrackJob.perform_later(name, id, Time.now.to_s)
   end
