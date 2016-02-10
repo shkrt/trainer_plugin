c0d174f2bab93fac822f6e70c9c5e77f684119ac
Shkrt <zxcgpppmn@gmail.com>
2016-02-10 18:50:08 +0300

    auth initial commit

--- app/controllers/trainer_plugin/application_controller.rb
+++ app/controllers/trainer_plugin/application_controller.rb
@@ -1,12 +1,27 @@
 module TrainerPlugin
   class ApplicationController < ActionController::Base
-    layout "trainer_plugin/application"
+    # attr_reader :current_user
+    include ActionController::HttpAuthentication::Basic::ControllerMethods
     before_filter :set_user_class
+    before_action :authenticate_basic
+    layout "trainer_plugin/application"
 
     private
 
     def set_user_class
       @user_class = TrainerPlugin.config.user_class.constantize
+    end
+
+    def authenticate_basic
+      logger.info("####DEBUG####")
+      logger.info(current_user)
+      # authenticate_with_http_basic do |email, _passwd|
+      #   if current_user
+      #     @current_user = user
+      #   else
+      #     render json: { message: "Authentication failed" }, status: 401
+      #   end
+      # end
     end
   end
 end
